76e91a6332a9c9a40007f1ea1fe1fcb35c4cc622
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2023-05-20 17:54:32 -0500

    added some actions in the profileuser controller and added some seed data to work with

--- app/controllers/profile_users_controller.rb
+++ app/controllers/profile_users_controller.rb
@@ -1,2 +1,19 @@
 class ProfileUsersController < ApplicationController
+
+    def show
+        user = ProfileUser.find_by(id: params[:id])
+        render json: user, status: :ok
+    end
+
+    def create
+        user = ProfileUser.create!(profile_user_params)
+        session[:user_id] = user.id
+        render json: user, status: :ok
+    end
+
+    private
+
+    def profile_user_params
+        params.permit(:username, :password, :avatar)
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+ProfileUser.create!(username: "JamesSalcedo001", password: "test", avatar: "https://images.squarespace-cdn.com/content/v1/54b7b93ce4b0a3e130d5d232/1519987020970-8IQ7F6Z61LLBCX85A65S/icon.png?format=1000w", admin: true)
