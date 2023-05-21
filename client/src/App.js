7118542ed8f4113ed4455a7ec2da4d741f65fa24
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2023-05-20 23:13:25 -0500

    added state and fetching user data to render an avatar and username for the user

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from "react"
 
 function App() {
+const [profileUser, setProfileUser] = useState([])
+
+useEffect(() => {
+  fetch(`profile_users/1`)
+  .then((res) => res.json())
+  .then((data) => setProfileUser(data))
+},[])
+
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Welcome!</h1>
+        <h1>Welcome {profileUser.username}!</h1>
+        <img className="image" src={profileUser.avatar} alt="avatar"/>
       </header>
     </div>
   );
